238f68d10ca62eaff6508ab97d7d8c2f7fc3eb6c
Mayankmodi2385 <mayankmodi7126@gmail.com>
2026-09-18 21:31:43 +0530

    first commit

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 const http = require ('http');
-http.createServer((req , res)=>{
-    res.write("<h1>hi i am mayank modi</h1>");
-
-res.end('hello');
-}).listen(4900);
+const age = 23;
+const server = http.createServer((req , res )=>{
+    res.setHeader("content-type","text/json");
+    res.write(
+        `<html>
+        <head>
+            <title>
+                NODEJS
+            </title>
+        </head>
+        <body>
+            <h1>hello my name is mayank</h1>
+            <h2>`+new Date+`</h2>
+            <h3>my age is `+age+`</h3>
+        </body>
+    </html>
+    `)
+    res.end();
+})
+server.listen(4900);
---
+++ simpleAPI.js
@@ -0,0 +1,24 @@
+const http = require('http');
+
+const userData=[
+{
+    name : 'mayank',
+    age : 23,
+    email : 'mayankmodi7126@gmail.com'
+},
+{
+    name : 'rahul',
+    age : 20,
+    email : 'rahul12@gmail.com'
+},
+{
+    name : 'raju',
+    age : 25,
+    email : 'raju26@gmail.com'
+}
+]
+const server=http.createServer((req , res )=>{
+
+    res.end("hi");
+})
+server.listen(5000);
